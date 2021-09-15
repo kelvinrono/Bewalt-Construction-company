df4ba5e38dfac0db3d72b7119884445aaaee0316
Rubyroy12 <ibrakipz7@gmail.com>
2021-09-15 12:35:19 +0300

    add templates

---
+++ gikpad/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    path('',views.index,name = 'index'),
+    path('about',views.about,name = 'about'),
+    path('services',views.services,name = 'services'),
+   
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
